6b5c6cf8560f9fba0cd2408d09c652dd659b113f
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-27 12:32:12 +0200

    string

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,53 @@
 ﻿
-Console.WriteLine("Hello, World!");
+
+//deklaracja zmiennej
+string greetings;
+//inicjalizację zmiennej
+greetings = "Hello, World!";
+//przypisanie wartości
+greetings = "Hello, Paweł!";
+
+Console.WriteLine(greetings);
+
+//deklarację + inicjalizacja
+string question = "Jak się masz?";
+
+string message = greetings + " " + question;
+
+//message = string.Format("{0} {1}", greetings, question);
+
+//string interpolowany
+message = $"{greetings} {question}";
+
+Console.WriteLine(message);
+
+
+string userName = AskForData("Podaj imię:");
+string userAge = AskForData("Podaj wiek:");
+Console.WriteLine($"Info o użytkowniku: {userName} ({userAge})");
+
+SayGoodbye(userName);
+
+//budowa metody
+//<typ zwracany> <nazwa metody>(<typ opcjonalnego parametru> <nazwa opcjonalnego parametru>)
+string AskForData(string message)
+{
+    Console.WriteLine(message);
+    string userInput = Console.ReadLine();
+    
+    //zwracamy rezultat z funkcji
+    return userInput;
+}
+//string AskForData()
+//{
+//    Console.WriteLine("Podaj imię:");
+//    string userInput = Console.ReadLine();
+
+//    return userInput;
+//}
+
+//void - funkcja nic nie zwraca
+void SayGoodbye(string name)
+{
+    Console.WriteLine($"Bye {name}!");
+}

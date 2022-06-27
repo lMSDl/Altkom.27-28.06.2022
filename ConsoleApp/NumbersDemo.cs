5f6c81165ab2659af35693aef72b40f2aed6905d
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-27 16:09:25 +0200

    Numbers

---
+++ ConsoleApp/NumbersDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class NumbersDemo
+    {
+
+        public void Run()
+        {
+            int a = 17;
+            int b = 10;
+            int c = 5;
+
+            int d = a - 3 * c + a * b;
+
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            Console.WriteLine($"a - 3 * c + a * b = {d}");
+
+            int e = c + c * c;
+            Console.WriteLine($"c + c * c = {e}");
+
+
+            float f = a / b;
+            Console.WriteLine($"a / b = {f}");
+
+            int g =  (int)Math.Round(6 * 0.25, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"6 * 0.25 = {g}");
+
+            double h = 3.3333;
+            long i = long.MaxValue;
+
+            i = a; //niejawne rzutowanie z typu o mniejszej prezycji na typ o większej precyzji
+            a = (int)i; //jawne rzutowanie na typ o mniejszej prezycji
+
+            h = f;
+            f = (float)h;
+
+            Console.WriteLine("Podaj wartość a:");
+            string value =  Console.ReadLine();
+            a = int.Parse(value);
+
+
+            Console.WriteLine("Podaj wartość b:");
+            value = Console.ReadLine();
+            b = int.Parse(value);
+
+            Console.WriteLine("Podaj wartość f:");
+            value = Console.ReadLine();
+            f = float.Parse(value);
+
+
+            Console.WriteLine($"a + b + f = {a+b+f}");
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,12 @@
 ﻿//USING - deklaracja, że będziemy korzystać z klas wskazanej przestrzeni nazw
 using ConsoleApp;
 
+NumbersDemo numbersDemo = new NumbersDemo();
+numbersDemo.Run();
+
+
+
+
 
 StringDemo stringDemo = new StringDemo();
 stringDemo.Run();
@@ -11,3 +17,5 @@ Console.WriteLine($"{person.FirstName} {person.LastName}");
 
 //person.FirstName = "Monika"; //nie możemy, bo setter jest prywatny
 person.LastName = "Monikowska";
+
+

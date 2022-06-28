e7909a1590f63150ab4e5803048a373f7ccc8a74
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-28 09:20:07 +0200

    konstruktor teleskopowy

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     public class Person
     {
-        public Person(string lastName, string firstName)
+        public Person(int age)
+        {
+            Age = age;
+        }
+        public Person(string lastName, string firstName, int age) : this(lastName, firstName)
+        {
+            Age = age;
+        }
+        public Person(string lastName, string firstName) : this(firstName)
         {
             LastName = lastName;
+        }
+        public Person(string firstName)
+        {
             FirstName = firstName;
         }
 
         public string LastName { get; set; }
         public string FirstName { get; }
-
+        public int Age { get; set; }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,17 +2,18 @@
 using ConsoleApp;
 
 NumbersDemo numbersDemo = new NumbersDemo();
-numbersDemo.Run();
+//numbersDemo.Run();
 
 
 
 
 
 StringDemo stringDemo = new StringDemo();
-stringDemo.Run();
+//stringDemo.Run();
 
 
-Person person = new Person("Ewowska", "Ewa");
+//Person person = new Person("Ewowska", "Ewa");
+Person person = new Person("Ewa") { Age = 12, LastName = "Ewowska"}; // {} - inicjalizator pozwala na uzupełnienie właściwości po konstruktorze, ale przed uzyskaniem obiektu
 Console.WriteLine($"{person.FirstName} {person.LastName}");
 
 //person.FirstName = "Monika"; //nie możemy, bo setter jest prywatny
